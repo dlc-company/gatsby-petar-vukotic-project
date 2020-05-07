5adafac71fc3a60b60632d1707b5dd0d881da67c
Milan Josipovic <milan.josipovic.git@gmail.com>
2020-05-07 17:35:01 +0200

    feat:reciklaza-page

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,21 +4,41 @@ exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
   const { data } = await graphql(`
-    query {
-      posts:allContentfulBiomasa{
-        edges{
-          node{
-            slug
-            }
-          }
+  query{
+    posts: allContentfulBiomasa {
+      edges {
+        node {
+          slug
         }
+      }
     }
+    
+    recycleposts: allContentfulReciklaza {
+      edges {
+        node {
+          slug
+        }
+      }
+    }
+  }
+  
   `)
 
   data.posts.edges.forEach(({ node }) => {
     createPage({
       path: `biomasa/${node.slug}`,
       component: path.resolve("./src/templates/blog-template.js"),
+      context: {
+        slug: node.slug,
+      },
+    })
+  })
+
+
+  data.posts.edges.forEach(({ node }) => {
+    createPage({
+      path: `reciklaza/${node.slug}`,
+      component: path.resolve("./src/templates/blog-template-recycle.js"),
       context: {
         slug: node.slug,
       },
--- src/pages/reciklaza.js
+++ src/pages/reciklaza.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Layout from "../components/Layout"
-
 import StyledHero from '../components/StyledHero'
 import {graphql} from 'gatsby'
-
+import BlogListRecycle from '../components/Blog/BlogListRecycle'
+import ReciklazaAbout from '../components/Home/ReciklazaAbout'
 const reciklaza = ({data}) => {
     return (
         <Layout>
              <StyledHero img={data.blogBcg.childImageSharp.fluid} />
+             <ReciklazaAbout />
+             <BlogListRecycle />
         </Layout>
     )
 }
