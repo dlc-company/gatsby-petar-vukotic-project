c5bada7107ded6c962ab89544d0926658585989b
miladintomic996 <miladintomic996@gmail.com>
2020-07-13 08:39:58 +0200

    survey style

--- src/components/Home/Survey.js
+++ src/components/Home/Survey.js
@@ -10,22 +10,30 @@ const SurveyComponent = () => {
 var model = new Survey.Model(surveyJson);
  return (
    <div className={styles.main}>
-     <p className={styles.title} >Anketa</p>      
-     <div id='surveySection' className={styles.surveySection}>   
-        <div className = {styles.surveyImage}>
-         <img id='surveyPlaceholder' src={image}></img>
-         <div className={styles.survey} id='survey'>
-           < Survey.Survey model={model} onComplete={sendDataToServer} />
-         </div>    
-         <h1 className={styles.completedText} id='completed'>Hvala!</h1>       
-        </div>
+     <div className={styles.title}>
+       <Title subtitle="anketa"></Title>
+     </div>
+     <div id="surveySection" className={styles.surveySection}>
+       <div className={styles.surveyImage}>
+         <img id="surveyPlaceholder" src={image} className={styles.image}></img>
+         <div className={styles.survey} id="survey">
+           <Survey.Survey model={model} onComplete={sendDataToServer} />
+         </div>
+         <h1 className={styles.completedText} id="completed">
+           Hvala!
+         </h1>
+       </div>
        <div className={styles.surveyTextButton}>
-         <h6 className={styles.text} >Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores corporis, distinctio perspiciatis rem cupiditate nobis, commodi similique sed consectetur minima eos fugit quo? Assumenda, ducimus?</h6>  
+         <h6 className={styles.text}>
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+           corporis, distinctio perspiciatis rem cupiditate nobis, commodi
+           similique sed consectetur minima eos fugit quo? Assumenda, ducimus?
+         </h6>
          <button onClick={showSurvey} type="button" className="btn-primary">
            Popuni Anketu
-        </button> 
-        </div>    
-      </div>
+         </button>
+       </div>
+     </div>
    </div>
  )
 }
