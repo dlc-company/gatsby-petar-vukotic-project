f3e0d3561d91890036d0718e8f478ce188bfaabc
miladintomic996 <miladintomic996@gmail.com>
2020-08-04 11:04:23 +0200

    us page

--- src/components/Home/UdruzenjaSumovlasnikaAbout.js
+++ src/components/Home/UdruzenjaSumovlasnikaAbout.js
@@ -6,22 +6,24 @@ import styled from 'styled-components'
 
 const UdruzenjaSumovlasnikaAbout = () => {
  return (
-  <section className={styles.about}>
-
-   <div className={styles.aboutCenter}>
-
-    <article className={styles.aboutInfo}>
-     <Title title="udruženja" subtitle="sumovlasnika" />
-     <p>
-      <span>Udruženja šumovlsnika</span> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio eos, dolore dolores officiis accusamus porro laudantium perferendis nobis culpa impedit sint praesentium quam quisquam possimus consequatur, beatae omnis, vero ex!</p>
-
-
-
-
-
-    </article>
-   </div>
-  </section>
+   <section className={styles.about}>
+     <div className={styles.aboutCenter}>
+       <article className={styles.aboutInfo}>
+         <Title title="udruženja" subtitle="sumovlasnika" />
+         <p>
+           Najoptimalnije i najfunkcionalnije
+           rešavanje svih (ili većine) problema u privatnim šumama (posebno onih
+           čiji vlasnici ne žive u blizini svojih šuma) je osnivanje udruženja.
+           Članom 73 Zakona o šuma je definisano:„Udruženje iz stava 1. ovog
+           člana koje poseduje više od 100 ha šume, bez obzira na ukupnu
+           površinu šuma u svojini, ima prioritet pri korišćenju sredstva
+           Budžetskog fonda.“ Članovi Udruženja ostaju vlasnici svojih šuma,
+           udruživanjm ništa ne gube, samo mogu dobiti, a ako nisu zadovoljni,
+           mogu i izaći iz udruženja.
+         </p>
+       </article>
+     </div>
+   </section>
  )
 }
 
