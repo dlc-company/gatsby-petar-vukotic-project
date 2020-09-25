fd2cc5477d01460846489511c91c4b1752bb2a8c
milan-josipovic <milan.josipovic@protonmail.com>
2020-09-25 14:29:21 +0200

    Contact text changed

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,21 +4,21 @@ import styles from '../../css/contact.module.css'
 const Contact = () => {
     return (
         <section className={styles.contact}>
-            <Title title="contact" subtitle='us' />
+            <Title title="pišite" subtitle='nam' />
             <div className={styles.center}>
                 <form  action="https://formspree.io/mvowpnek" method="POST" className={styles.form}>
                     <div>
                         {/* Labels su druga opcija ako zelis */}
-                        <input type="text" name="name" id="name" className={styles.formControl} placeholder="ime i prezime"/>
+                        <input type="text" name="name" id="name" className={styles.formControl} placeholder="Ime & Prezime"/>
                     </div>
                     <div>
                         <input type="email" name="email" id="email" className={styles.formControl} placeholder="email@email.com"/>
                     </div>
                     <div>
-                        <textarea name="message" id='message' rows="10" className={styles.formControl} placeholder="hello" />
+                        <textarea name="message" id='message' rows="10" className={styles.formControl} placeholder="" />
                     </div>
                     <div>
-                        <input type="submit" value="submit" className={styles.submit} />
+                        <input type="submit" value="pošalji" className={styles.submit} />
                     </div>   
                 </form>
             </div>
